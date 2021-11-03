8a5c512ba8c21934bc22da82184e29e4c3576371
limseongeun <lseeee@naver.com>
2021-11-03 16:42:24 +0900

    file install

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            sass: {
+            data: `
+                @import "@/assets/scss/style.scss";
+            `
+            }
+        }
+    }
+}
+  
