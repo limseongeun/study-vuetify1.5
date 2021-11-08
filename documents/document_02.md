a840749749053f01067d60093aa9759fa69625b3
limseongeun <lseeee@naver.com>
2021-11-08 17:15:36 +0900

    eslint & prettier set

--- documents/document_02.md
+++ documents/document_02.md
@@ -1,7 +1,19 @@
 # 2강 에디터(VScode) 설정 및 Vuetify1.5 설치
 
+설치한 에디터 'Visual Studio Code'에서 vue 작업하는 데 필수적인 Extensions을 설정하는 방법과 프로젝트에 맞는 Vuetify1.5를 설치하는 방법에 대한 내용입니다.
 
+## 1. Visual Studio Code 설정
 
-*****
+-   폴더에서 VScode 열기 - 커맨드 창에서 다음과 같이 입력
+
+```
+code .
+```
+
+-   메뉴에서 'View - Extenstions'에서 Vue 관련 프로그램 설치
+
+---
+
 ### [목록으로 이동](README.md)
+
 ### [다음 3.Firebase에 사이트 배포하기](document_03.md)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,21 @@
 <template>
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.png')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-    </v-layout>
-  </v-container>
+	<v-container>
+		<v-layout text-xs-center wrap>
+			<v-flex xs12>
+				<v-img
+					:src="require('../assets/logo.png')"
+					class="my-3"
+					contain
+					height="200"
+				></v-img>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
 
-
 <script>
-
 export default {
-    name: "Home",
-    components: {
-        
-    },
-}
+	name: 'Home',
+	components: {},
+};
 </script>
